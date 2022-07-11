03ededb84e7f2f735c90a494510a10ae9f4bfcdd
Trần Văn Bắc <bacne@email.com>
2022-07-11 16:33:04 +0700

    update project

--- client_scan_party/src/assets/icons/index.jsx
+++
@@ -1,7 +0,0 @@
-import { ReactComponent as VietNamIcon} from './viet-nam-icon.svg';
-import { ReactComponent as EnglishIcon} from './english-icon.svg';
-
-export {
-    VietNamIcon,
-    EnglishIcon
-}
--- client_scan_party/src/stores/reduxFormStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers } from 'redux'
-import { reducer as formReducer } from 'redux-form'
-
-const reducers = {
-  form: formReducer  
-}
-const reducer = combineReducers(reducers)
-const store = createStore(reducer)
-export default store;
